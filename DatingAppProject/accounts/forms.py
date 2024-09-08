8176b73b975b64b984d9f069b1eb8ce94c7bde64
jithin <jithinbalqn@gmail.com>
2024-09-08 14:11:24 +0530

    JobDetailsForm-forms.py updated

--- DatingAppProject/accounts/forms.py
+++ DatingAppProject/accounts/forms.py
@@ -105,3 +105,33 @@ class PersonalDetailsForm(forms.ModelForm):
        self.fields['qualification'].empty_label = "Qualification"
 
 
+class JobDetailsForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['company_name','designation','work_location']
+        widgets = {
+            'company_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': ' Company Name',
+            }),
+
+            'designation': forms.Select(attrs={
+                'class': 'form-control',
+                'placeholder': ' Designation',
+            }),
+            'work_location': forms.Select(attrs={
+                'class': 'form-control',
+                'placeholder': ' Work Location',
+            }),
+        }
+
+        labels = {
+            'company_name': '',
+            'designation': ' ',
+            'work_location': ' ',
+        }
+    def __init__(self, *args, **kwargs):
+       super(JobDetailsForm, self).__init__(*args, **kwargs)
+       self.fields['company_name'].empty_label = "Company Name"
+       self.fields['designation'].empty_label = "Designation"
+       self.fields['work_location'].empty_label = "Work Location"
