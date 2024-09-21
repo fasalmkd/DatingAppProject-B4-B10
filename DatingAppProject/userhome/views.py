fe2e681830e4cce8a204ccb777a10a819f0c3caf
prajina kk <prajinakk7@gmail.com>
2024-09-21 15:09:54 +0900

    home.html file updated

--- DatingAppProject/userhome/views.py
+++ DatingAppProject/userhome/views.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomeView(TemplateView):
-    # template_name="shared/leftright_base.html"
+    # template_name="shared/sidebars.html"
     template_name = "home.html"
 
 class EntryView(TemplateView):
-    # template_name="shared/leftright_base.html"
+    # template_name="shared/sidebars.html"
     template_name = "entry.html"
