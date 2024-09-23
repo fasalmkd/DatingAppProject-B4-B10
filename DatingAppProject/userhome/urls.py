484aac9be8ce8d7f4ca50d5afa94ba5d7d211496
prajina kk <prajinakk7@gmail.com>
2024-09-23 16:48:47 +0900

    39 and homepage backend completed story part backend left

--- DatingAppProject/userhome/urls.py
+++ DatingAppProject/userhome/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . views import *
 
+app_name = 'userhome'
+
 urlpatterns = [
     path('home/', HomeView.as_view(),name="home1"),
     path('entry',EntryView.as_view(),name="entry")
